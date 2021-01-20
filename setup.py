5f468d2c3e197e6510d7765381b6c4e0d45751c2
Eric Denovellis <edeno@bu.edu>
2021-01-20 15:26:25 -0800

    Update dependencies

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy', 'numba', 'scipy', 'scikit-learn', 'matplotlib',
-                    'xarray', 'pandas', 'dask', 'tqdm', 'statsmodels', 'patsy']
+INSTALL_REQUIRES = ['numpy >= 1.11', 'scipy', 'opencv-python', 'tqdm']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
